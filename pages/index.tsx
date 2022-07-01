d1326905972d7d11373b7c41efd1b82d8248fb2a
Burak Kantarcı <burak@thundra.io>
2022-07-01 14:46:01 +0300

    another in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import axios from 'axios'
 
 import styles from '@/pages/index.module.css'
 
@@ -62,4 +63,22 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export function Another() {
+  console.log("hi there");
+
+  axios.get('https://randomuser.me/api/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 }
