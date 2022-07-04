685dfe70fb7cc04b13cb4af61cdf0815c94632b7
Burak Kantarcı <burak@thundra.io>
2022-07-04 14:01:32 +0300

    added a log to puppeteer

--- __tests__/google.spec.ts
+++ __tests__/google.spec.ts
@@ -4,6 +4,7 @@ describe("Google.com", () => {
   it('should get correct title when go to home page', async () => {
     await page.goto('https://google.com');    
     const title = await page.title();    
+    console.log(await title);
     expect(title).toEqual('Google');
   });
 });
