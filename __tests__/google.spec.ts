38abaf521bf10dbd5c5fe212636f5d7aa633fd9f
Burak Kantarci <burakkantarci.08@gmail.com>
2022-07-06 11:14:18 +0300

    added a failed test

--- __tests__/google.spec.ts
+++ __tests__/google.spec.ts
@@ -8,3 +8,12 @@ describe("Google.com", () => {
     expect(title).toEqual('Google');
   });
 });
+
+describe("Google.com", () => {
+  it('should get wrong title when go to home page', async () => {
+    await page.goto('https://google.com');    
+    const title = await page.title();    
+    console.log(await title);
+    expect(title).toEqual('Yahoo');
+  });
+});
